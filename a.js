cc445524b98490f4601cf22883467cf0b1e59cb0
Paradox <tananhdt99@gmail.com>
2022-08-25 09:38:52 +0700

    add file

---
+++ a.js
@@ -0,0 +1,36 @@
+class Calculator {
+  calculate(number1, number2, method) {
+  //do smt using method with number1 and number2
+  return method.cal(number1, number2);
+  }
+};
+
+class Addition {
+ cal(number1, number2) {
+    return number1 + number2;
+  }
+};
+
+class Subtraction {
+  cal(number1, number2) {
+    return number1 - number2;
+  }
+};
+
+class Multiplication {
+  cal(number1, number2) {
+    return number1 * number2;
+  }
+};
+
+class Division {
+  cal(number1, number2) {
+    return number1 / number2;
+  }
+};
+
+
+let calculator = new Calculator();
+let addition = new Addition();
+let abc = calculator.calculate(1,2, addition);
+console.log(abc);
